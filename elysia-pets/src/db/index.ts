db35e33a3750dfdfe62e4e9728f4d50a42558da6
Joe Kim <joesm.genie@gmail.com>
2025-02-18 14:37:26 +0700

    Add Drizzle ORM configuration and database connection setup

---
+++ elysia-pets/src/db/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+const queryClient = postgres(process.env.DATABASE_URL!);
+const db = drizzle({
+    client: queryClient,
+});
+
+export default db;
