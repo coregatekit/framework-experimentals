22406e3cb5cd6eb8593df834e88b1aaf0d33f52a
Joe Kim <joesm.genie@gmail.com>
2025-02-18 14:55:47 +0700

    Add database credentials configuration to Drizzle ORM setup

--- elysia-pets/drizzle.config.ts
+++ elysia-pets/drizzle.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/db/schemas",
   dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  }
 });
